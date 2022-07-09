64fa71f87963547a56a8b4ae9ba2ec82f17ad5ed
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-07-09 12:08:42 -0300

    Simplify and optimize xr

--- src/common/xr.py
+++ src/common/xr.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import json
 import os
 import time
 
+import orjson
 from fasteners import InterProcessReaderWriterLock
 
 from common import url
@@ -11,19 +11,18 @@ from common import url
 session = url.Session()
 
 
-def main(base: str, quote: str) -> float:
+def conv(base: str, quote: str) -> float:
     """Cache data from APILayer's Exchange Rates API for 8 hours."""
 
-    # FIXME: global monthly rate limit, not per base currency
+    # FIXME: global monthly limit, not per base currency: fix with USD as base
     limit = 250 // 30 * 60 * 60
 
     cache = f"{os.environ['XDG_RUNTIME_DIR']}/xr.{base.lower()}.json"
-    rw_lock = InterProcessReaderWriterLock(cache)
+    cache_lock = InterProcessReaderWriterLock(cache)
 
     if os.path.exists(cache) and os.path.getmtime(cache) + limit >= time.time():
-        with rw_lock.read_lock():
-            with open(cache) as fd:
-                data = json.load(fd)
+        with cache_lock.read_lock(), open(cache) as fd:
+            data = orjson.loads(fd.read())
     else:
         reply = session.get(
             f"https://api.apilayer.com/exchangerates_data/latest?base={base}",
@@ -31,23 +30,7 @@ def main(base: str, quote: str) -> float:
         )
         data = reply.json()
 
-        with rw_lock.write_lock():
-            with open(cache, "w") as fd:
-                json.dump(data, fd)
+        with cache_lock.write_lock(), open(cache, "w") as fd:
+            fd.write(orjson.dumps(data))
 
     return data["rates"][quote]
-
-
-def usd(quote: str = "BRL") -> float:
-    """Return exchange rates quoted against USD."""
-    return main("USD", quote)
-
-
-def eur(quote: str = "BRL") -> float:
-    """Return exchange rates quoted against EUR."""
-    return main("EUR", quote)
-
-
-def brl(quote: str = "USD") -> float:
-    """Return exchange rates quoted against BRL."""
-    return main("BRL", quote)
