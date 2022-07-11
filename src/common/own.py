7ef722f970e0926230329bd3d576d37bcdb14344
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-07-11 16:04:08 -0300

    Comment on available chassis names

--- src/common/own.py
+++ src/common/own.py
@@ -4,5 +4,10 @@ import socket
 
 from common import cmd
 
+# From `man hostnamectl`: Currently, the following chassis types are defined: "desktop",
+# "laptop", "convertible", "server", "tablet", "handset", "watch", "embedded", as well
+# as the special chassis types "vm" and "container" for virtualized systems that lack an
+# immediate physical chassis.
 chassis: str = cmd.run("LC_ALL=C hostnamectl | awk '/Chassis/{print $2}'").stdout
+
 hostname: str = socket.gethostname()
