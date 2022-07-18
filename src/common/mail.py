375798fad94145c3c65b6e383e091c2441ae73ff
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-07-18 02:53:48 -0300

    Dump email contents if no MTA is found

--- src/common/mail.py
+++ src/common/mail.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+import logging
 from shlex import quote
+from shutil import which
 
 from common import cmd, own
+
+log = logging.getLogger(__name__)
 
 
 def mail(subject: str, message: str, address: str = "26373564-goldencedar@users.noreply.gitlab.com") -> None:
     """Email using existing system MTA."""
+    if not which("mail"):
+        log.warning(
+            f"""Command "mail" not found, dumping intended message:
+            {subject}
+            {message}
+            """
+        )
+        return
+
     cmd.run(
         # if performance matters, shell=False won't start a new shell
         ["mail", "-s", f"[{own.hostname}] {quote(subject)}", address],
