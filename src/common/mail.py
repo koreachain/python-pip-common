2a42acfd84f15d948778547175895954ec272577
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-07-18 14:42:37 -0300

    Strip leading and trailing newlines from emails

--- src/common/mail.py
+++ src/common/mail.py
@@ -13,15 +13,16 @@ def mail(subject: str, message: str, address: str = "26373564-goldencedar@users.
     """Email using existing system MTA."""
     if not which("mail"):
         log.warning(
-            f"""Command "mail" not found, dumping intended message:
+            f"""
+            Command "mail" not found, dumping intended message:
             {subject}
             {message}
-            """
+            """.strip()
         )
         return
 
     cmd.run(
         # if performance matters, shell=False won't start a new shell
         ["mail", "-s", f"[{own.hostname}] {quote(subject)}", address],
-        input=message.encode(),
+        input=message.strip().encode(),
     )
