f4f4dede7b957b394228bbdc83db9761e29e2d0c
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-08-09 01:49:09 -0300

    Remove module included by accident

--- src/common/ts.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-
-def sec(s: int) -> str:
-    """Convert elapsed time in seconds to timestamp."""
-    M, S = divmod(s, 60)
-    H, M = divmod(M, 60)
-    D, H = divmod(H, 24)
-    return f"{D:d}d{H:02d}h{M:02d}m{S:02d}s"
