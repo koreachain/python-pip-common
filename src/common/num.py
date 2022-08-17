669e8f39e3550cb01561055e22fc7e799c058c99
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-08-17 13:03:59 -0300

    Include num.exp_range() for exponential ranges

--- src/common/num.py
+++ src/common/num.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
 from math import isclose
+from typing import Type
+
+import numpy as np
 
 
-def approx(a, b, rel=1e-9, abs=1e-12) -> bool:
-    "Handle numbers close to 0 in math.isclose()."
+def approx(
+    a: int | float, b: int | float, rel: float = 1e-9, abs: float = 1e-12
+) -> bool:
+    """Handle numbers close to 0 in math.isclose()."""
     return isclose(a, b, rel_tol=rel, abs_tol=abs)
+
+
+def exp_range(
+    start: int | float,
+    stop: int | float,
+    count: int,
+    ntype: Type[int | float] | None = None,
+    truncate: int | None = None,
+    unique: bool = False,
+) -> tuple[int | float, ...]:
+    """Return exponential range of ints or floats."""
+    if not ntype:
+        ntype = int if isinstance(start, int) and isinstance(stop, int) else float
+
+    scale = np.geomspace(start, stop, count)
+
+    if ntype is int:
+        scale = np.around(scale).astype(int)
+    elif truncate:
+        scale = np.around(scale, decimals=truncate)
+
+    if unique:
+        scale = np.unique(scale)
+
+    native = tuple(x.item() for x in scale)
+    return native
