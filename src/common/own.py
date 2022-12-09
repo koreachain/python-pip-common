b0aac42c25f574031282f0e25a7c5448a12163ae
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2022-12-09 19:47:07 +0900

    Implement own.ip and own.country (ISO ALPHA-2)

--- src/common/own.py
+++ src/common/own.py
@@ -3,6 +3,8 @@
 import os
 import socket
 
+import requests
+
 from common import cmd
 
 # From `man hostnamectl`: Currently, the following chassis types are defined: "desktop",
@@ -22,3 +24,30 @@ else:
     chassis = "unknown"
 
 hostname: str = socket.gethostname()
+
+ip: str
+country: str
+
+
+def _ip() -> str:
+    global ip
+    reply = requests.get("https://checkip.amazonaws.com")
+    ip = reply.text.strip()
+
+    return ip
+
+
+def _country() -> str:
+    global country
+    reply = requests.get("https://api.iplocation.net", params={"ip": _ip()})
+    country = reply.json()["country_code2"]
+
+    return country
+
+
+def __getattr__(name: str):
+    if name == "ip":
+        return _ip()
+    elif name == "country":
+        return _country()
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
