dfcd0ebb3a579bc4847c956740db96a63a93e82f
Silver Aloe <26373564-silveraloe@users.noreply.gitlab.com>
2023-03-07 21:32:12 -0300

    Make retrying optional

--- src/common/xr.py
+++ src/common/xr.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
 
+import logging
 import os
 import time
 
+import fastlogging
 import orjson
+import requests
 from fasteners import InterProcessReaderWriterLock
 
 from common import url
 
+if "root" in fastlogging.domains:
+    log = fastlogging.domains["root"]
+else:
+    log = logging.getLogger(__name__)
+
+
 session = url.Session()
 
 
-def conv(base: str, quote: str) -> float:
+def _read_cached(cache, cache_lock):
+    with cache_lock.read_lock(), open(cache) as fd:
+        return orjson.loads(fd.read())
+
+
+def conv(base: str, quote: str, retry=True) -> float:
     """Cache data from APILayer's Exchange Rates API for 8 hours."""
 
     # FIXME: global monthly limit, not per base currency: fix with USD as base
@@ -21,16 +35,25 @@ def conv(base: str, quote: str) -> float:
     cache_lock = InterProcessReaderWriterLock(cache)
 
     if os.path.exists(cache) and os.path.getmtime(cache) + limit >= time.time():
-        with cache_lock.read_lock(), open(cache) as fd:
-            data = orjson.loads(fd.read())
+        data = _read_cached(cache, cache_lock)
     else:
-        reply = session.get(
-            f"https://api.apilayer.com/exchangerates_data/latest?base={base}",
-            headers={"apikey": "FUmdHFwAguPbLI8eWnhD8EbyAeYoOrQj"},
-        )
-        data = reply.json()
+        try:
+            reply = (session if retry else requests).get(
+                f"https://api.apilayer.com/exchangerates_data/latest?base={base}",
+                headers={"apikey": "FUmdHFwAguPbLI8eWnhD8EbyAeYoOrQj"},
+                timeout=5,
+            )
+        except Exception as e:
+            if os.path.exists(cache):
+                log.warning(f"{type(e).__name__}: {e}, use cached {base}/{quote} rate")
+                data = _read_cached(cache, cache_lock)
+            else:
+                log.error(f"Failed to fetch {base}/{quote} exchange rate, not cached")
+                raise
+        else:
+            data = reply.json()
 
-        with cache_lock.write_lock(), open(cache, "wb") as fd:
-            fd.write(orjson.dumps(data))
+            with cache_lock.write_lock(), open(cache, "wb") as fd:
+                fd.write(orjson.dumps(data))
 
     return data["rates"][quote]
